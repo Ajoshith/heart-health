4d6930ebeb794c5dbdbcf958731b00082610a648
sathwikbalu <dlsathwik@gmail.com>
2023-12-28 14:35:21 +0530

    m

--- server.js/userSchema.js
+++ server.js/userSchema.js
@@ -1,58 +1,58 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const k="hello_world"
+const k = "hello_world";
 
 const userSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      
-       // Adjust the minimum length as needed
-    },
-    tokens: [
-      {
-        token:{
-          type:String,
-          required:true
-        }
-      }
-    ],
-    medicalHistory: {
-      age:{type:Number},
-      sex:{type:Number},
-      cp:{type:Number},
-      rbp:{type:Number},
-      sc:{type:Number},
-      fbs:{type:Number},
-      rer:{type:Number},
-      mhr:{type:Number},
-      eia:{type:Number},
-      olds:{type:Number},
-      st:{type:Number},
-      mvs:{type:Number}
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
   },
+  password: {
+    type: String,
 
-  });
-userSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
+    // Adjust the minimum length as needed
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  medicalHistory: {
+    age: { type: Number },
+    sex: { type: Number },
+    cp: { type: Number },
+    trestbps: { type: Number },
+    chol: { type: Number },
+    fbs: { type: Number },
+    restecg: { type: Number },
+    thalach: { type: Number },
+    exang: { type: Number },
+    oldpeak: { type: Number },
+    slope: { type: Number },
+    ca: { type: Number },
+    thal: { type: Number },
+  },
+});
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 userSchema.methods.generateAuthToken = async function () {
-    const token = jwt.sign({ _id: this._id },"hello world");
-    this.tokens = this.tokens.concat({ token: token });
-    await this.save();
-   
-    return token;
+  const token = jwt.sign({ _id: this._id }, "hello world");
+  this.tokens = this.tokens.concat({ token: token });
+  await this.save();
+
+  return token;
 };
 
-const User = mongoose.model('articles', userSchema);
+const User = mongoose.model("articles", userSchema);
 module.exports = User;
