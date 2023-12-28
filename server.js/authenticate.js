9c60e9de0daf7dbc57195236e32993d4f0cb04b9
sathwikbalu <dlsathwik@gmail.com>
2023-12-28 14:22:00 +0530

    m

--- server.js/authenticate.js
+++ server.js/authenticate.js
@@ -1,16 +1,18 @@
-const jwt=require("jsonwebtoken")
-const User=require("./userSchema")
-const Authentication= (req,resp,next)=>{
+const jwt = require("jsonwebtoken");
+const User = require("./userSchema");
+const Authentication = (req, resp, next) => {
+  try {
     const token = req.cookies.jwtoken;
-    if(token){
-        const result= jwt.verify(token,'hello world')
-        req.userId= result._id
-        console.log(999)
-    }
-    else{
-        req.userId='false';
+    if (token) {
+      const result = jwt.verify(token, "hello world");
+      req.userId = result._id;
+      console.log(999);
+    } else {
+      req.userId = "false";
     }
     next();
-
-}
-module.exports=Authentication;
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = Authentication;
